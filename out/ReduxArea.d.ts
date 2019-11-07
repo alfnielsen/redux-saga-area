a164946c11c0ccb64968c8c0d4542a6692b3817c
alfnielsen <alfnielsen@gmail.com>
2019-11-07 22:51:07 +0100

    build v 0.1.0

--- out/ReduxArea.d.ts
+++ out/ReduxArea.d.ts
@@ -5,33 +5,81 @@ declare type ReduxAction = ((...args: any) => AnyAction) & {
     name: string;
     reducer: Reducer;
 };
-declare const CreateReduxArea: <TState, D = Draft<TState>>(initialState: TState) => {
+declare type AreaAction<TState, T extends Func> = ((...args: Parameters<T>) => ReturnType<T> & {
+    type: string;
+}) & {
+    name: string;
+    reducer: Reducer<Immutable<TState>, ReturnType<T> & {
+        type: string;
+    }>;
+    type: ReturnType<T> & {
+        type: string;
+    };
+};
+declare type AreaActionEmpty<TState> = (() => {
+    type: string;
+}) & {
+    name: string;
+    reducer: Reducer<Immutable<TState>, {
+        type: string;
+    }>;
+    type: {
+        type: string;
+    };
+};
+interface ICreateReduxAreaOptions {
+    namePrefix?: string;
+    fetchPostfix?: string[];
+}
+declare const CreateReduxArea: <TState>(initialState: TState) => {
+    namePrefix: string;
+    fetchPostfix: string[];
+    options: (options: ICreateReduxAreaOptions) => any;
     add: (name: string) => {
-        action: <T extends Func>(action: T) => {
-            produce: (producer: (draft: Draft<TState>, action: ReturnType<T> & {
+        produce: (producer: (draft: Draft<TState>, action: {
+            type: string;
+        }) => void) => AreaActionEmpty<TState>;
+        reducer: (reducer: (state: TState, reducerAction: {
+            type: string;
+        }) => any) => (() => {
+            type: string;
+        }) & {
+            name: string;
+            reducer: Reducer<Immutable<TState>, {
                 type: string;
-            }) => void) => ((...args: Parameters<T>) => ReturnType<T> & {
+            }>;
+            type: {
                 type: string;
-            }) & {
-                name: string;
-                reducer: Reducer<Immutable<TState>, ReturnType<T> & {
-                    type: string;
-                }>;
-                type: ReturnType<T> & {
-                    type: string;
-                };
             };
-            reducer: (reducer: (state: TState, reducerAction: ReturnType<T> & {
+        };
+        action: <TAction extends Func>(action: TAction) => {
+            produce: (producer: (draft: Draft<TState>, action: ReturnType<TAction> & {
                 type: string;
-            }) => any) => ((...args: Parameters<T>) => ReturnType<T> & {
+            }) => void) => AreaAction<TState, TAction>;
+            reducer: (reducer: (state: TState, reducerAction: ReturnType<TAction> & {
                 type: string;
-            }) & {
-                name: string;
-                reducer: Reducer<Immutable<TState>, ReturnType<T> & {
-                    type: string;
-                }>;
-                type: ReturnType<T> & {
-                    type: string;
+            }) => any) => AreaAction<TState, TAction>;
+        };
+    };
+    addFetch: (name: string) => {
+        action: <TFetchAction extends Func>(action: TFetchAction) => {
+            produce: (producer: (draft: Draft<TState>, action: ReturnType<TFetchAction> & {
+                type: string;
+            }) => void) => {
+                successAction: <TSuccessAction extends Func>(successAction: TSuccessAction) => {
+                    successProduce: (successProducer: (draft: Draft<TState>, action: ReturnType<TSuccessAction> & {
+                        type: string;
+                    }) => void) => {
+                        failureAction: <TFailureAction extends Func>(failureAction: TFailureAction) => {
+                            failureProduce: (failureProducer: (draft: Draft<TState>, action: ReturnType<TFailureAction> & {
+                                type: string;
+                            }) => void) => {
+                                fetch: AreaAction<TState, TFetchAction>;
+                                success: AreaAction<TState, TSuccessAction>;
+                                failure: AreaAction<TState, TFailureAction>;
+                            };
+                        };
+                    };
                 };
             };
         };
--- out/ReduxArea.js
+++ out/ReduxArea.js
@@ -1,33 +1,128 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const immer_1 = require("immer");
+const actionMethod = (name, action) => {
+    const actionCreator = (...args) => (Object.assign({}, action.apply(null, args), { type: name }));
+    const mappedAction = actionCreator;
+    Object.defineProperty(mappedAction, 'name', {
+        value: name,
+        writable: false
+    });
+    return mappedAction;
+};
+const produceMethod = (mappedAction, producer) => {
+    Object.defineProperty(mappedAction, 'reducer', {
+        value: immer_1.default(producer),
+        writable: false
+    });
+    return mappedAction;
+};
+const produceMethodEmpty = (name, producer) => {
+    const mappedAction = (() => ({ type: name }));
+    Object.defineProperty(mappedAction, 'reducer', {
+        value: immer_1.default(producer),
+        writable: false
+    });
+    Object.defineProperty(mappedAction, 'name', {
+        value: name,
+        writable: false
+    });
+    return mappedAction;
+};
+const reduceMethod = (mappedAction, reducer) => {
+    Object.defineProperty(mappedAction, 'reducer', {
+        value: reducer,
+        writable: false
+    });
+    return mappedAction;
+};
+const reduceMethodEmpty = (name, reducer) => {
+    const mappedAction = (() => ({ type: name }));
+    Object.defineProperty(mappedAction, 'reducer', {
+        value: reducer,
+        writable: false
+    });
+    Object.defineProperty(mappedAction, 'name', {
+        value: name,
+        writable: false
+    });
+    return mappedAction;
+};
 const CreateReduxArea = (initialState) => {
     const actions = [];
-    return {
+    const area = {
+        namePrefix: '',
+        fetchPostfix: ['Fetch', 'Success', 'Failure'],
+        options: (options) => {
+            if (options.namePrefix !== undefined) {
+                area.namePrefix = options.namePrefix;
+            }
+            if (options.fetchPostfix !== undefined) {
+                area.fetchPostfix = options.fetchPostfix;
+            }
+            return area;
+        },
         add: (name) => ({
+            produce: (producer) => {
+                const mappedAction = produceMethodEmpty(area.namePrefix + name, producer);
+                actions.push(mappedAction);
+                return mappedAction;
+            },
+            reducer: (reducer) => {
+                const mappedAction = reduceMethodEmpty(area.namePrefix + name, reducer);
+                actions.push(mappedAction);
+                return mappedAction;
+            },
             action: (action) => {
-                const actionCreator = (...args) => (Object.assign({}, action.apply(null, args), { type: name }));
-                const mappedAction = actionCreator;
-                Object.defineProperty(mappedAction, 'name', {
-                    value: name,
-                    writable: false
-                });
+                let mappedAction = actionMethod(area.namePrefix + name, action);
                 return {
                     produce: (producer) => {
-                        Object.defineProperty(mappedAction, 'reducer', {
-                            value: immer_1.default(producer),
-                            writable: false
-                        });
+                        mappedAction = produceMethod(mappedAction, producer);
                         actions.push(mappedAction);
                         return mappedAction;
                     },
                     reducer: (reducer) => {
-                        Object.defineProperty(mappedAction, 'reducer', {
-                            value: reducer,
-                            writable: false
-                        });
+                        mappedAction = reduceMethod(mappedAction, reducer);
                         actions.push(mappedAction);
                         return mappedAction;
+                    }
+                };
+            }
+        }),
+        addFetch: (name) => ({
+            action: (action) => {
+                let mappedAction = actionMethod(area.namePrefix + name + area.fetchPostfix[0], action);
+                return {
+                    produce: (producer) => {
+                        mappedAction = produceMethod(mappedAction, producer);
+                        actions.push(mappedAction);
+                        return {
+                            successAction: (successAction) => {
+                                let mappedSuccessAction = actionMethod(area.namePrefix + name + area.fetchPostfix[1], successAction);
+                                return {
+                                    successProduce: (successProducer) => {
+                                        mappedSuccessAction = produceMethod(mappedSuccessAction, successProducer);
+                                        actions.push(mappedSuccessAction);
+                                        return {
+                                            failureAction: (failureAction) => {
+                                                let mappedFailureAction = actionMethod(area.namePrefix + name + area.fetchPostfix[2], failureAction);
+                                                return {
+                                                    failureProduce: (failureProducer) => {
+                                                        mappedFailureAction = produceMethod(mappedFailureAction, failureProducer);
+                                                        actions.push(mappedSuccessAction);
+                                                        return {
+                                                            fetch: mappedAction,
+                                                            success: mappedSuccessAction,
+                                                            failure: mappedFailureAction
+                                                        };
+                                                    }
+                                                };
+                                            }
+                                        };
+                                    }
+                                };
+                            }
+                        };
                     }
                 };
             }
@@ -42,6 +137,7 @@ const CreateReduxArea = (initialState) => {
         actions,
         initialState
     };
+    return area;
 };
 exports.default = CreateReduxArea;
 //# sourceMappingURL=ReduxArea.js.map
